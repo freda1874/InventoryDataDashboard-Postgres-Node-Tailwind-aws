7c58bdeaca649f993d8715598b4aea8755043df3
Luo Freda <freda.luo@gmail.com>
2024-10-10 19:10:33 -0400

    changed configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,7 +28,7 @@ app.use("/products", productRoutes); // http://localhost:8000/products
 app.use("/users", userRoutes); // http://localhost:8000/users
 app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
 // server 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
 });
